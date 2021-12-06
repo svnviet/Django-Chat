b9dc0d33bfe8a468a89604e12c662b4c7b3a80a3
Vietnt <svnviet@gmail.com>
2021-12-06 17:05:20 +0700

    eidt

--- text_to_speech/forms.py
+++ text_to_speech/forms.py
@@ -2,14 +2,14 @@ from django import forms
 
 # from django.contrib.auth.models import User
 
-VoiceList = [(1, 'A - Giọng Nữ - Miền Bắc'),
-             (2, 'B - Giọng Nam - Miền Bắc'),
-             (3, 'C - Giọng Nữ - Miền Bắc'),
-             (4, 'D - Giọng Nam - Miền Bắc'),
-             (5, 'A - Giọng Nữ - Miền Nam'),
-             (6, 'B - Giọng Nam - Miền Nam'),
-             (7, 'C - Giọng Nữ - Miền Nam'),
-             (8, 'D - Giọng Nam - Miền Nam')]
+VoiceList = [(1, 'Ngọc Hân - Giọng Nữ - Miền Bắc'),
+             (2, 'Quang Tuấn - Giọng Nam - Miền Bắc'),
+             (3, 'Ngọc Diệp - Giọng Nữ - Miền Bắc'),
+             (4, 'Hoàng Anh - Giọng Nam - Miền Bắc'),
+             (5, 'Ngọc Linh - Giọng Nữ - Miền Bắc'),
+             (6, 'Hoàng Nam - Giọng Nam - Miền Bắc'),
+             (7, 'Đặng Loan - Giọng Nữ - Miền Trung'),
+             (8, 'Phương Nam - Giọng Nam - Miền Trung')]
 
 SpeedList = [(0.25, '0.25'), (0.5, '0.5'), (0.75, '0.75'), (1, '1'), (1.25, '1.25'), (1.5, '1.5'), (1.75, '1.75'), (2, '2')]
 
